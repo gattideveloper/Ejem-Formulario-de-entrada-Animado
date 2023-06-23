cffefdd48891efeab9a4f12c11ad6a0f2ee13bb8
GattiDev <gattidev323@gmail.com>
2023-06-23 09:17:06 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<!-- Para que funcione en los navegadores viejos -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- Para los celulares, para que no hagan zoom -->
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<title>GattiDev : : Animated Input Form</title>
+		<!-- Icon -->
+		<link rel="icon" type="favicon/x-icon" href="materials/icon/favicon.ico" />
+		<!-- Styles -->
+		<link rel="stylesheet" type="text/css" href="css/styles.css" />
+	</head>
+	<body>
+		<div class="input-box">
+            <input type="text" class="name-input" required />
+            <label class="name-label">First Name</label>
+        </div>
+        <div class="input-box">
+            <input type="text" class="name-input" required />
+            <label class="name-label">Last Name</label>
+        </div>		
+	</body>
+</html>
